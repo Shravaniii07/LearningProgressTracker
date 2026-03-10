48e24d996d46d4d8882e3ddac4c318f1d5d8d051
Shravaniii07 <shravanibodare@gmail.com>
2026-03-10 14:24:51 +0530

    deleted frontend folder

---
+++ backend/model/lang.js
@@ -0,0 +1,29 @@
+const mongoose=require('mongoose');
+
+const LangSchema=new mongoose.Schema({
+LanguageTitle:{
+type:String,
+required:true
+},
+BookAuthor:{
+type:String,
+required:true
+},
+PublishDate:{
+type:Date,
+required:true
+},
+Price:{
+type:Number,
+required:true
+},
+Description:{
+type:String,
+required:false
+}
+
+},{timestamps:true})
+
+const book=mongoose.model("book",bookSchema);
+
+module.exports=book;
